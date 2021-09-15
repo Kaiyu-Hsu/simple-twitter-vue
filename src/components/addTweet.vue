30c02ae2391689779788842e8acfb08e1d6e0fb1
Armogo <taro37940@yahoo.com.tw>
2021-09-16 02:04:28 +0800

    adjust seed file

--- src/components/addTweet.vue
+++ src/components/addTweet.vue
@@ -144,7 +144,7 @@ textarea {
 </style>
 
 <script>
-import tweetsJSON from "./../../public/tweets.json";
+import tweetsJSON from "./../../public/api-tweets.json";
 
 const currentUser = tweetsJSON.userData;
 export default {
