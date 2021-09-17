f171fbc7f86f5023ad64be984465470cd43b4ae3
Armogo <taro37940@yahoo.com.tw>
2021-09-18 05:24:50 +0800

    WIP User.vue

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="home">
-    <Navbar />
+    <Navbar :initial-user="userData" />
     <div class="profile">
       <!-- ShowUser -->
       <ShowUser @open-edit-modal="openModal" />
       <!-- UserTabs -->
       <UserTabs />
       <!-- 推文 -->
-      <router-view />
+      <router-view :initial-user="userData" />
     </div>
     <!-- edit modal -->
     <UserEditModal v-if="isModalVisible" @close="closeModal" />
     <Popular />
+    <ReplyModal
+      @open-reply-modal="openReplyModal"
+      @close-reply-modal="closeReplyModal"
+      :tweet-content="oneTweet"
+      :initial-user="userData"
+    />
   </div>
 </template>
 
@@ -24,7 +30,6 @@
   bottom: 0px;
   width: 42%;
   height: 100%;
-  z-index: -1;
 }
 </style>
 
@@ -33,7 +38,9 @@ import Popular from "./../components/Popular";
 import Navbar from "./../components/Navbar";
 import ShowUser from "./../components/ShowUser";
 import UserTabs from "./../components/UserTabs";
-import UserEditModal from "../components/UserEditModal.vue";
+import UserEditModal from "./../components/UserEditModal.vue";
+import ReplyModal from "./../components/ReplyModal.vue"
+import data from "../../public/api-users-id-v2.json";
 
 export default {
   name: "User",
@@ -43,19 +50,41 @@ export default {
     ShowUser,
     UserTabs,
     UserEditModal,
+    ReplyModal
   },
   data() {
     return {
+      userData: {},
+      userTweets: [],
+      popular: [],
       isModalVisible: false,
+      isReplyModalVisible: false,
     };
   },
   methods: {
+    fetchData() {
+      this.userData = {
+        ...data,
+        ...data.userData,
+      };
+      this.userTweets = [...data.userTweets];
+      this.popular = [...data.popular];
+    },
     openModal() {
       this.isModalVisible = true;
     },
     closeModal() {
       this.isModalVisible = false;
     },
+    openReplyModal() {
+      this.isReplyModalVisible = true;
+    },
+    closeReplyModal() {
+      this.isReplyModalVisible = false;
+    },
+  },
+  created() {
+    this.fetchData();
   },
 };
 </script>
