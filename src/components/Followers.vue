bbdc91962c3a4e101639d908fa5a61eb421a08db
Kaiyu <anne550323@gmail.com>
2021-09-25 00:35:13 +0800

    fix: 種子資料有null的問題 refactor: style

--- src/components/Followers.vue
+++ src/components/Followers.vue
@@ -1,113 +1,110 @@
 <template>
   <div class="followers">
     <h1>跟隨誰</h1>
-    <!-- TODO 資料渲染有問題 -->
     <div v-for="user in users" :key="user.followingId" class="popular-users">
-      <!-- <img :src="user.following.avatar" alt="avatar" /> -->
-      <img
-        src="https://loremflickr.com/320/240/restaurant,food/?random=9.52029547879647&lock=1.8356969306294824"
-        alt="avatar"
-      />
+      <img :src="user.following.avatar" alt="avatar" />
       <div class="name-account">
-        <!-- {{ user.following.name }} -->
-        <div class="name">user5</div>
-        <!--  {{ user.following.account }} -->
-        <div class="account">@ Vel r</div>
+        <div class="name">{{ user.following.name }}</div>
+        <div class="account">@ {{ user.following.account }}</div>
       </div>
-      <!-- <div
+      <div
         v-if="user.isFollowed"
         @click.stop.prevent="toggleFollowing(user.followingId)"
         class="following-btn"
-      > -->
-      <div class="following-btn">正在跟隨</div>
-      <!-- <div
+      >
+        正在跟隨
+      </div>
+      <div
         v-else
         @click.stop.prevent="toggleFollowing(user.followingId)"
         class="unfollowing-btn"
-      > -->
-      <div class="unfollowing-btn">跟隨</div>
+      >
+        跟隨
+      </div>
     </div>
-    <footer @click="showMore">顯示更多</footer>
+    <footer @click="showMore">
+      <span>顯示更多</span>
+    </footer>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .followers {
-  position: fixed;
+  position: absolute;
   top: 15px;
-  left: 75%;
+  left: 630px;
   background: #f5f8fa;
   width: 350px;
   border-radius: 14px;
   font-family: "Noto Sans TC", sans-serif;
-}
-
-h1 {
-  font-size: 18px;
-  padding-left: 15px;
-}
-
-.popular-users {
-  display: flex;
   font-size: 15px;
   font-weight: 700;
-  align-items: center;
-  justify-content: space-around;
-  padding: 10px;
-  border-top: 2px solid #e6ecf0;
-}
 
-img {
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-  margin: 10px;
-}
+  h1 {
+    font-size: 18px;
+    padding-left: 15px;
+  }
 
-.name-account {
-  margin-right: 80px;
-}
+  .popular-users {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    padding: 10px;
+    border-top: 1px solid #e6ecf0;
 
-.account {
-  color: #657786;
-}
+    img {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      margin: 10px;
+      background: #c4c4c4;
+    }
 
-.following-btn,
-.unfollowing-btn {
-  cursor: pointer;
-}
+    .name-account {
+      margin-right: 80px;
+      .account {
+        color: #657786;
+      }
+    }
 
-.following-btn {
-  width: 90px;
-  height: 35px;
-  background: #ff6600;
-  border: 1px solid #ff6600;
-  box-sizing: border-box;
-  border-radius: 100px;
-  font-family: Noto Sans TC;
-  font-size: 15px;
-  color: #ffffff;
+    .following-btn,
+    .unfollowing-btn,
+    span {
+      cursor: pointer;
+    }
 
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-}
+    .following-btn,
+    .unfollowing-btn {
+      border: 1px solid #ff6600;
+      box-sizing: border-box;
+      border-radius: 100px;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+    }
 
-.unfollowing-btn {
-  width: 60px;
-  height: 35px;
-  border: 1px solid #ff6600;
-  box-sizing: border-box;
-  border-radius: 100px;
-  font-family: Noto Sans TC;
-  font-size: 15px;
-  color: #ff6600;
+    .following-btn {
+      width: 90px;
+      height: 35px;
+      background: #ff6600;
+      color: #ffffff;
+    }
 
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
+    .unfollowing-btn {
+      width: 60px;
+      height: 35px;
+      color: #ff6600;
+    }
+  }
+
+  footer {
+    font-style: normal;
+    font-weight: normal;
+    color: #ff6600;
+    padding: 12px 0px 12px 15px;
+    border-top: 1px solid #e6ecf0;
+  }
 }
 </style>
 
@@ -128,7 +125,6 @@ export default {
           isFollowed: true,
         };
       });
-      console.log(this.users[0].following.avatar); // TODO 證明 following 裡可以顯示的
     },
     toggleFollowing(userId) {
       this.users = this.users.map((user) => {
@@ -143,11 +139,11 @@ export default {
       });
     },
     showMore() {
+      // TODO 顯示更多追隨者
       console.log("showMore");
     },
   },
   created() {
-    console.log("created");
     this.fetchJSON();
   },
 };
