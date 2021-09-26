2d09d9a1a63459e538a2cda12dde06c974d59bed
Kaiyu <anne550323@gmail.com>
2021-09-26 21:58:54 +0800

    del: JSON.parse 因原本就是string

--- src/api/tweets.js
+++ src/api/tweets.js
@@ -1,16 +1,16 @@
-import { apiHelper } from "../utils/helpers";  
+import { apiHelper } from "../utils/helpers";
 
 export const tweets = {
   getTweets() {
-    const token = JSON.parse(localStorage.getItem("token"))
-    
+    const token = localStorage.getItem("token");
+
     // 這裡 return 的會是一個 Promise
     return apiHelper.get("api/tweets", {
       headers: { Authorization: `Bearer ${token}` },
     });
   },
   getReply(id) {
-    const token = JSON.parse(localStorage.getItem("token"));
+    const token = localStorage.getItem("token");
 
     // 這裡 return 的會是一個 Promise
     return apiHelper.get(`api/tweets/${id}/replies`, {
@@ -18,14 +18,14 @@ export const tweets = {
     });
   },
   postTweets(id, description, paramsId) {
-    const token = JSON.parse(localStorage.getItem("token"))
+    const token = localStorage.getItem("token");
     // TODO
     return apiHelper.post(
       "api/tweets",
       {
         user: { id }, // user.id
         body: { description }, // body.comment
-        params: {paramsId}// params.id
+        params: { paramsId }, // params.id
       },
       {
         headers: { Authorization: `Bearer ${token}` },
@@ -33,8 +33,6 @@ export const tweets = {
     );
   },
   postReply(id) {
-    apiHelper.post(`api/tweets/${id}/replies`,{
-
-    })
-  }
+    apiHelper.post(`api/tweets/${id}/replies`, {});
+  },
 };
--- src/api/user.js
+++ src/api/user.js
@@ -2,49 +2,49 @@ import { apiHelper } from "../utils/helpers";
 
 export default {
   getUserInfo(id) {
-    const token = JSON.parse(localStorage.getItem("token"));
+    const token = localStorage.getItem("token");
 
     return apiHelper.get(`api/users/${id}/userinfo`, {
       headers: { Authorization: `Bearer ${token}` },
     });
   },
   getEditUser(id) {
-    const token = JSON.parse(localStorage.getItem("token"));
+    const token = localStorage.getItem("token");
 
     return apiHelper.get(`api/users/${id}`, {
       headers: { Authorization: `Bearer ${token}` },
     });
   },
   putEditUser(id, body) {
-    const token = JSON.parse(localStorage.getItem("token"));
+    const token = localStorage.getItem("token");
 
     return apiHelper.put(`api/users/${id}`, body, {
       headers: { Authorization: `Bearer ${token}` },
     });
   },
   getFollowers(id) {
-    const token = JSON.parse(localStorage.getItem("token"));
+    const token = localStorage.getItem("token");
 
     return apiHelper.get(`api/users/${id}/followers`, {
       headers: { Authorization: `Bearer ${token}` },
     });
   },
   getFollowings(id) {
-    const token = JSON.parse(localStorage.getItem("token"));
+    const token = localStorage.getItem("token");
 
     return apiHelper.get(`api/users/${id}/followings`, {
       headers: { Authorization: `Bearer ${token}` },
     });
   },
   getReplied(id) {
-    const token = JSON.parse(localStorage.getItem("token"));
+    const token = localStorage.getItem("token");
 
     return apiHelper.get(`api/users/${id}/replied_tweets`, {
       headers: { Authorization: `Bearer ${token}` },
     });
   },
   getPopular(id) {
-    const token = JSON.parse(localStorage.getItem("token"));
+    const token = localStorage.getItem("token");
 
     return apiHelper.get(`api/tweets/${id}/top10`, {
       headers: { Authorization: `Bearer ${token}` },
