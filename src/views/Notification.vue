d430fbef9b98e37156b33fb5b5899d6a207ba85a
Kaiyu <anne550323@gmail.com>
2022-01-06 15:36:26 +0800

    change: Followers is changed for Popular

--- src/views/Notification.vue
+++ src/views/Notification.vue
@@ -7,7 +7,7 @@
       </div>
       <div>
         <div class="a-note">
-          <img src="" alt="" />
+          <img src="http://img.yao51.com/jiankangtuku/qnkswqwpy.jpeg" alt="" />
           <div class="note">John 有新的推文通知</div>
           <div class="content">
             Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
@@ -17,47 +17,46 @@
         </div>
       </div>
     </div>
-    <Followers />
+    <Popular />
+    <!-- <Followers /> -->
   </div>
 </template>
 
 <style lang="scss" scoped>
-.container {
+.notification {
+  width: 42%;
   position: absolute;
-  left: 378px;
+  left: 0px;
   height: 100%;
-  bottom: 0px;
+  margin-left: 27%;
   border-left: 1px solid #e6ecf0;
   border-right: 1px solid #e6ecf0;
   font-family: Noto Sans TC;
   font-style: normal;
   font-weight: bold;
-  .notification {
-    width: 600px;
-    .tiltle {
-      font-size: 18px;
-      padding: 15px 0px 15px 15px;
-      border-bottom: 1px solid #e6ecf0;
+  .tiltle {
+    font-size: 18px;
+    padding: 15px 0px 15px 15px;
+    border-bottom: 1px solid #e6ecf0;
+  }
+  .a-note {
+    border-bottom: 1px solid #e6ecf0;
+    padding: 10px 15px;
+    img {
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+      background: #c4c4c4;
     }
-    .a-note {
-      border-bottom: 1px solid #e6ecf0;
-      padding: 10px 15px;
-      img {
-        width: 30px;
-        height: 30px;
-        border-radius: 50%;
-        background: #c4c4c4;
-      }
-      .note {
-        margin: 5px 0px;
-        font-weight: 500;
-        font-size: 15px;
-      }
-      .content {
-        color: #657786;
-        font-weight: 500;
-        font-size: 15px;
-      }
+    .note {
+      margin: 5px 0px;
+      font-weight: 500;
+      font-size: 15px;
+    }
+    .content {
+      color: #657786;
+      font-weight: 500;
+      font-size: 15px;
     }
   }
 }
@@ -65,14 +64,16 @@
 
 <script>
 import Navbar from "./../components/Navbar.vue";
-import Followers from "./../components/Followers.vue";
+import Popular from "./../components/Popular";
+// import Followers from "./../components/Followers.vue";
 import { keepUnauthorizedOut, roleAccessControl } from "./../utils/helpers";
 
 export default {
   name: "Notification",
   components: {
     Navbar,
-    Followers,
+    Popular,
+    // Followers,
   },
   created() {
     keepUnauthorizedOut(this);
